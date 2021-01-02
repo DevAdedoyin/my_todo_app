51e8324fbc9bdb15a25a09af7e7f1912e67b69c7
Adedoyin <isradeoluwa@gmail.com>
2021-01-02 19:58:50 +0100

    Default Dummy Categories created

--- lib/providers/categories.dart
+++ lib/providers/categories.dart
@@ -2,16 +2,46 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Category {
-  final int id;
+  final String id;
   final Icon icon;
   final String title;
   final int numberOfList;
 
-  Category({this.id = 0, this.icon, this.title, this.numberOfList = 0});
+  Category(
+      {@required this.id,
+      @required this.icon,
+      @required this.title,
+      @required this.numberOfList});
 }
 
 class Categories with ChangeNotifier {
-  List<Category> _cat = [];
+  List<Category> _cat = [
+    Category(
+        id: 'cat1',
+        icon: Icon(Icons.wb_sunny_outlined),
+        title: 'My Day',
+        numberOfList: 0),
+    Category(
+        id: 'cat2',
+        icon: Icon(Icons.star_border_outlined),
+        title: 'Important',
+        numberOfList: 1),
+    Category(
+        id: 'cat3',
+        icon: Icon(Icons.all_inclusive),
+        title: 'All',
+        numberOfList: 4),
+    Category(
+        id: 'cat4',
+        icon: Icon(Icons.check_circle_outline_rounded),
+        title: 'Completed',
+        numberOfList: 0),
+    Category(
+        id: 'cat5',
+        icon: Icon(Icons.person_outline),
+        title: 'My Day',
+        numberOfList: 0),
+  ];
 
   List<Category> get cat {
     return [..._cat];
