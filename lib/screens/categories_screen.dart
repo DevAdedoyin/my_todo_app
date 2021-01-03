fd1509f99740b3d3f1811d236be80ee317a4dae5
Adedoyin <isradeoluwa@gmail.com>
2021-01-03 16:31:47 +0100

    Category Screen to main

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:my_todo_app/widgets/app_drawer.dart';
+
+class CategoryScreen extends StatefulWidget {
+  @override
+  _CategoryScreenState createState() => _CategoryScreenState();
+}
+
+class _CategoryScreenState extends State<CategoryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {
+                // TODO: OnPressed Feature Not Done
+              }),
+        ],
+      ),
+      drawer: AppDrawer(),
+      // TODO: User Category Widget Usage
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Center(
+          child: Column(
+            children: [
+              Icon(Icons.hourglass_empty_rounded),
+              Text("You have no ToDo List Category yet"),
+            ],
+          ),
+        ),
+      ]),
+    );
+  }
+}
