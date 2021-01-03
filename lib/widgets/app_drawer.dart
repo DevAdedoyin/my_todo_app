28cdc36fb5656c6162f048031cb150eab61355d2
Adedoyin <isradeoluwa@gmail.com>
2021-01-03 13:32:22 +0100

    App Drawer Built

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/categories.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final categories = Provider.of<Categories>(context);
+
     return Drawer(
       child: Column(
         children: <Widget>[
@@ -11,6 +15,22 @@ class AppDrawer extends StatelessWidget {
             automaticallyImplyLeading: false,
           ),
           Divider(),
+          Container(
+            height: 150,
+            child: ListView.builder(
+              itemBuilder: (_, i) => Column(
+                children: [
+                  ListTile(
+                    leading: Icon(categories.cat[i].icon),
+                    title: Text(categories.cat[i].title),
+                    trailing: Text(categories.cat[i].numberOfList.toString()),
+                  ),
+                  Divider(),
+                ],
+              ),
+              itemCount: categories.cat.length,
+            ),
+          )
         ],
       ),
     );
