7a9e09ac95842cf30b7dbb85899dec25f0dd177f
Adedoyin <isradeoluwa@gmail.com>
2021-01-03 14:16:55 +0100

    Main App Route Created

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:my_todo_app/screens/categories_screen.dart';
 
 void main() => runApp(MainApp());
 
-class MainApp extends StatefulWidget {
-  @override
-  _MainAppState createState() => _MainAppState();
-}
-
-class _MainAppState extends State<MainApp> {
+class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      title: 'My ToDo',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+        accentColor: Colors.yellow,
+      ),
+      home: CategoryScreen(),
     );
   }
 }
