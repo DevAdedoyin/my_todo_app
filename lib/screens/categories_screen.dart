7f402fa01179f9ecbc1985300cc5f210885fa679
Adedoyin <isradeoluwa@gmail.com>
2021-01-03 14:03:49 +0100

    Empty Category Screens Created

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:my_todo_app/widgets/app_drawer.dart';
+
+class CategoryScreen extends StatefulWidget {
+  @override
+  _CategoryScreenState createState() => _CategoryScreenState();
+}
+
+class _CategoryScreenState extends State<CategoryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {
+                // TODO: OnPressed Feature Not Done
+              }),
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: Center(
+        child: Column(
+          children: [
+            Icon(Icons.hourglass_empty_rounded),
+            Text("You have no ToDo List Category yet")
+          ],
+        ),
+      ),
+    );
+  }
+}
