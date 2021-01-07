f067102dad266e464a92c5f94d105616e9382c78
Adedoyin <isradeoluwa@gmail.com>
2021-01-07 13:18:20 +0100

    Dummy: Todo list detail screen created

--- lib/screens/todo_list_details_screen.dart
+++ lib/screens/todo_list_details_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:my_todo_app/providers/todo_list.dart';
+import 'package:my_todo_app/widgets/todo_list_details.dart';
 import 'package:provider/provider.dart';
+import '';
 
 class ToDoListDetailsScreen extends StatefulWidget {
   @override
@@ -10,60 +12,7 @@ class ToDoListDetailsScreen extends StatefulWidget {
 class _ToDoListDetailsScreenState extends State<ToDoListDetailsScreen> {
   @override
   Widget build(BuildContext context) {
-    final todoDets = Provider.of<ToDoProvider>(context);
-    return Scaffold(
-      body: CustomScrollView(
-        physics: const BouncingScrollPhysics(),
-        slivers: <Widget>[
-          SliverAppBar(
-            stretch: true,
-            onStretchTrigger: () {
-              return;
-            },
-            expandedHeight: 300.0,
-            flexibleSpace: FlexibleSpaceBar(stretchModes: <StretchMode>[
-              StretchMode.zoomBackground,
-              StretchMode.blurBackground,
-              StretchMode.fadeTitle,
-            ], centerTitle: true,
-            title: const Text('Title'),
-            background: Stack(fit: StackFit.expand, children: [
-              Image.network('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
-              fit: BoxFit.cover,
-              ),
- const DecoratedBox(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment(0.0, 0.5),
-                        end: Alignment(0.0, 0.0),
-                        colors: <Color>[
-                          Color(0x60000000),
-                          Color(0x00000000),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate([
-              ListTile(
-                leading: Icon(Icons.wb_sunny),
-                title: Text('Sunday'),
-                subtitle: Text('sunny, h: 80, l: 65'),
-              ),
-              ListTile(
-                leading: Icon(Icons.wb_sunny),
-                title: Text('Monday'),
-                subtitle: Text('sunny, h: 80,
-
-
-            ),
-          )
-        ],
-      ),
-    
+    //final todoDets = Provider.of<ToDoProvider>(context);
+    return Scaffold(body: SingleChildScrollView(child: ToDoDetailsWidget()));
   }
 }
