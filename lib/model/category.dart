efe4d469212c524040366b75824bafcaf8a9c584
Adedoyin <isradeoluwa@gmail.com>
2021-01-08 11:47:57 +0100

    Dummy: List of colors added to category model

--- lib/model/category.dart
+++ lib/model/category.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
+List<Color> color = [
+  Colors.yellowAccent,
+  Colors.blueGrey,
+  Colors.greenAccent,
+  Colors.grey,
+  Colors.redAccent,
+  Colors.deepOrangeAccent,
+  Colors.deepPurpleAccent
+];
+
 class Category {
   final String id;
   final IconData icon;
