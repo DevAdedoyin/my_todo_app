84cf992938fcf3768d9164849c0b9f1da6c4f379
Adedoyin <isradeoluwa@gmail.com>
2021-01-10 20:14:44 +0100

    Dummy: Custom Alert Dialog Widget Added to Category screen

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:my_todo_app/providers/categories.dart';
 import 'package:my_todo_app/widgets/app_drawer.dart';
+import 'package:my_todo_app/widgets/category_screen_widgets/alert_dialog_widget.dart';
 import 'package:my_todo_app/widgets/user_category_widget.dart';
 import 'package:provider/provider.dart';
 import '../model/category.dart';
@@ -12,27 +13,11 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
-  int selectedColor;
-  int index;
-
-  @override
-  void initState() {
-    super.initState();
-    selectedColor = 0;
-    index = 0;
-  }
-
-  void _selectedColor(int val) {
-    setState(() {
-      selectedColor = val;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     final userCats = Provider.of<Categories>(context);
     //final customCats = Provider.of<UserCategories>(context);
-    TextEditingController _textFieldController = TextEditingController();
+
     // Color selectedColor = bgColor[0];
 
     return Scaffold(
@@ -55,116 +40,7 @@ class _CategoryScreenState extends State<CategoryScreen> {
               splashColor: Colors.white,
               onPressed: () {
                 showDialog(
-                    context: context,
-                    builder: (_) {
-                      return AlertDialog(
-                        title: Text(
-                          'Category',
-                        ),
-                        content: StatefulBuilder(builder:
-                            (BuildContext context, StateSetter stateSetter) {
-                          return Container(
-                            height: 160,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Container(
-                                  child: Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      Expanded(
-                                        flex: 1,
-                                        child: Icon(
-                                          Icons.category,
-                                          size: 20,
-                                          color: bgColors[selectedColor],
-                                        ),
-                                      ),
-                                      Expanded(
-                                        flex: 7,
-                                        child: Container(
-                                            margin: EdgeInsets.only(left: 10),
-                                            child: TextField(
-                                              controller: _textFieldController,
-                                              decoration: InputDecoration(
-                                                hintText: 'Enter your category',
-                                                // border: InputBorder.none,
-                                                // focusedBorder: InputBorder.none,
-                                                // enabledBorder: InputBorder.none,
-                                                // errorBorder: InputBorder.none,
-                                                // disabledBorder: InputBorder.none,
-                                              ),
-                                            )),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                                Container(
-                                  margin: EdgeInsets.only(top: 10),
-                                  child: Chip(
-                                    backgroundColor: bgColors[selectedColor],
-                                    label: Text(
-                                      'Color',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                  ),
-                                ),
-                                Container(
-                                  height: 50,
-                                  // margin: EdgeInsets.only(top: 1),
-                                  child: ListView.builder(
-                                    scrollDirection: Axis.horizontal,
-                                    itemBuilder: (_, i) => Container(
-                                      height: 30,
-                                      width: 30,
-                                      margin: EdgeInsets.all(5),
-                                      decoration: BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        color: bgColors[i],
-                                      ),
-                                      //TODO: Convert List of Colors and name to a map
-                                      child: Radio(
-                                        activeColor: bgColors[i],
-                                        value: i,
-                                        groupValue: selectedColor,
-                                        onChanged: (val) {
-                                          stateSetter(() {
-                                            _selectedColor(val);
-                                            index = val;
-                                          });
-                                        },
-                                      ),
-                                    ),
-                                    itemCount: bgColor.length,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          );
-                        }),
-                        elevation: 7,
-                        actions: [
-                          IconButton(
-                            icon: Icon(Icons.cancel_rounded),
-                            onPressed: () {},
-                            color: Colors.red,
-                            iconSize: 30,
-                            splashColor: Colors.redAccent,
-                            splashRadius: 25,
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.check_circle),
-                            onPressed: () {},
-                            color: Colors.green,
-                            iconSize: 30,
-                            splashColor: Colors.greenAccent,
-                            splashRadius: 25,
-                          ),
-                        ],
-                      );
-                    });
+                    context: context, builder: (_) => AlertDialogWidget());
               },
               child: Row(
                 children: [
