b13890fa0f604302400948303c0594336d2eb66a
Adedoyin <isradeoluwa@gmail.com>
2021-01-15 11:19:23 +0100

    Dummy: Add List Widget Created for showing modal bottom sheet items

--- lib/screens/todo_list_screen.dart
+++ lib/screens/todo_list_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:my_todo_app/model/category.dart';
 import 'package:my_todo_app/providers/todo_list.dart';
-import 'package:my_todo_app/widgets/todo_list_widget.dart';
+import 'package:my_todo_app/widgets/todo_list_screen_widgets/add_list_widget.dart';
+import 'package:my_todo_app/widgets/todo_list_screen_widgets/todo_list_widget.dart';
 import 'package:provider/provider.dart';
 
 class TodoListScreen extends StatefulWidget {
   static const routeName = '/todo-list';
+
   @override
   _TodoListScreenState createState() => _TodoListScreenState();
 }
@@ -76,91 +78,11 @@ class _TodoListScreenState extends State<TodoListScreen> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           return showModalBottomSheet(
+              isDismissible: true,
               context: context,
+              elevation: 10,
               isScrollControlled: true,
-              builder: (context) => Padding(
-                    padding: MediaQuery.of(context).viewInsets,
-                    child: Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Row(
-                            children: [
-                              Expanded(
-                                flex: 4,
-                                child: Container(
-                                  margin: EdgeInsets.only(left: 15, right: 10),
-                                  child: TextField(
-                                    autofocus: true,
-                                    decoration: InputDecoration(
-                                      border: InputBorder.none,
-                                      hintText: 'Add a task',
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Expanded(
-                                flex: 1,
-                                child: Icon(Icons.save_alt_rounded),
-                              )
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Expanded(
-                                flex: 1,
-                                child: FlatButton(
-                                  padding: EdgeInsets.only(left: 5, right: 5),
-                                  child: Row(
-                                    children: [
-                                      Expanded(
-                                        child: Icon(Icons.alarm),
-                                      ),
-                                      Text('Remind me')
-                                    ],
-                                  ),
-                                  onPressed: () {},
-                                ),
-                              ),
-                              Expanded(
-                                flex: 1,
-                                child: FlatButton(
-                                  padding: EdgeInsets.only(left: 5, right: 5),
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      Expanded(
-                                        child: Icon(Icons.repeat),
-                                      ),
-                                      Text('Repeat')
-                                    ],
-                                  ),
-                                  onPressed: () {},
-                                ),
-                              ),
-                              Expanded(
-                                flex: 1,
-                                child: FlatButton(
-                                  padding: EdgeInsets.only(left: 5, right: 5),
-                                  child: Row(
-                                    children: [
-                                      Expanded(
-                                        child:
-                                            Icon(Icons.calendar_today_rounded),
-                                      ),
-                                      Text('Set due date')
-                                    ],
-                                  ),
-                                  onPressed: () {},
-                                ),
-                              )
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                  ));
+              builder: (context) => AddListWidget());
         },
         backgroundColor: Theme.of(context).primaryColor,
         splashColor: Colors.white54,
---
+++ lib/widgets/todo_list_screen_widgets/add_list_widget.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class AddListWidget extends StatefulWidget {
+  @override
+  _AddListWidgetState createState() => _AddListWidgetState();
+}
+
+class _AddListWidgetState extends State<AddListWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: MediaQuery.of(context).viewInsets,
+      child: Container(
+        // width: 350,
+        padding: EdgeInsets.only(left: 10, right: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Container(
+                    // margin: EdgeInsets.only(left: 15, right: 10),
+                    child: TextField(
+                      autofocus: true,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Add a task',
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  // flex: 1,
+                  child: Icon(Icons.save_alt_rounded),
+                )
+              ],
+            ),
+            Container(
+              // width: 350,
+              child: Row(
+                children: [
+                  Expanded(
+                    // flex: 1,
+                    child: FlatButton(
+                      // padding: EdgeInsets.only(left: 5, right: 5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [Icon(Icons.alarm), Text('Remind me')],
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Expanded(
+                    // flex: 1,
+                    child: FlatButton(
+                      // padding: EdgeInsets.only(left: 5, right: 5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [Icon(Icons.repeat), Text('Repeat')],
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      // height: 30,
+                      // width: 20,
+                      decoration: BoxDecoration(shape: BoxShape.circle),
+                      child: FlatButton(
+                        // padding: EdgeInsets.only(left: 5, right: 5),
+                        child: Row(
+                          children: [
+                            Icon(Icons.calendar_today_rounded),
+                            Text('Set due date')
+                          ],
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
