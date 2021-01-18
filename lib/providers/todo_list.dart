599c5525e8c86cdcf24dde3cc7654a9204aa61c8
Adedoyin <isradeoluwa@gmail.com>
2021-01-18 19:07:26 +0100

    Dummy 2: All hardcoded Todo List Removed

--- lib/providers/todo_list.dart
+++ lib/providers/todo_list.dart
@@ -18,64 +18,7 @@ class Todo {
 }
 
 class ToDoProvider with ChangeNotifier {
-  Map<String, Todo> _todo = {
-    '1': Todo(
-        date: DateTime.now().month.toString(),
-        frequency: 'repeat',
-        title: 'Flutter App',
-        time: DateTime.now().hour.toString(),
-        note:
-            "This just a test note or you can call it a dummy note. Nothing special for now"),
-    '2': Todo(
-        date: DateTime.now().month.toString(),
-        frequency: 'repeat',
-        title: 'Flutter App',
-        time: DateTime.now().hour.toString(),
-        note:
-            "This just a test note or you can call it a dummy note. Nothing special for now"),
-    '3': Todo(
-        date: DateTime.now().month.toString(),
-        frequency: 'repeat',
-        title: 'Flutter App',
-        time: DateTime.now().hour.toString(),
-        note:
-            "This just a test note or you can call it a dummy note. Nothing special for now"),
-    '4': Todo(
-        date: DateTime.now().month.toString(),
-        frequency: 'repeat',
-        title: 'Flutter App',
-        time: DateTime.now().hour.toString(),
-        note:
-            "This just a test note or you can call it a dummy note. Nothing special for now"),
-    '5': Todo(
-        date: DateTime.now().month.toString(),
-        frequency: 'repeat',
-        title: 'Flutter App',
-        time: DateTime.now().hour.toString(),
-        note:
-            "This just a test note or you can call it a dummy note. Nothing special for now"),
-    '6': Todo(
-        date: DateTime.now().month.toString(),
-        frequency: 'repeat',
-        title: 'Flutter App',
-        time: DateTime.now().hour.toString(),
-        note:
-            "This just a test note or you can call it a dummy note. Nothing special for now"),
-    '7': Todo(
-        date: DateTime.now().month.toString(),
-        frequency: 'repeat',
-        title: 'Flutter App',
-        time: DateTime.now().hour.toString(),
-        note:
-            "This just a test note or you can call it a dummy note. Nothing special for now"),
-    '8': Todo(
-        date: DateTime.now().month.toString(),
-        frequency: 'repeat',
-        title: 'Flutter App',
-        time: DateTime.now().hour.toString(),
-        note:
-            "This just a test note or you can call it a dummy note. Nothing special for now"),
-  };
+  Map<String, Todo> _todo = {};
 
   Map<String, Todo> get todo {
     return {..._todo};
