f0aaeb2a817008f07ed15a05cd845bf33dd75b8b
Adedoyin <isradeoluwa@gmail.com>
2021-01-26 17:52:52 +0100

    Database library changed from sqflite to moor

--- lib/model/app_database.dart
+++ lib/model/app_database.dart
@@ -1,11 +1,11 @@
-import 'package:flutter/material.dart';
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
-import 'dart:async';
+// import 'package:flutter/material.dart';
+// import 'package:path/path.dart';
+// import 'package:sqflite/sqflite.dart';
+// import 'dart:async';
 
-final Future<Database> database = getDatabasesPath().then((String path) {
-  return openDatabase(join(path, 'app_database.db'), onCreate: (db, version) {
-    db.execute(
-        'CREATE TABLE category(categoryId INTEGER PRIMARY KEY autoincrement, categoryTitle TEXT, numberOfList INTEGER, isFavorite INTEGER, color INTEGER)');
-  });
-});
+// final Future<Database> database = getDatabasesPath().then((String path) {
+//   return openDatabase(join(path, 'app_database.db'), onCreate: (db, version) {
+//     db.execute(
+//         'CREATE TABLE category(categoryId INTEGER PRIMARY KEY autoincrement, categoryTitle TEXT, numberOfList INTEGER, isFavorite INTEGER, color INTEGER)');
+//   });
+// });
