33c6184d1c3c860a7c3b64fee3eabb964443d019
Adedoyin <isradeoluwa@gmail.com>
2021-01-27 03:19:49 +0100

    Primary Key for database tables created

--- lib/model/app_database.dart
+++ lib/model/app_database.dart
@@ -1,19 +1,28 @@
 import 'package:moor/moor.dart';
+import 'dart:io';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+import 'package:moor/ffi.dart';
 
 part 'app_database.g.dart';
 
 @DataClassName('Category')
 class Category extends Table {
   IntColumn get categoryId => integer().autoIncrement()();
-  TextColumn get categoryTitle => text().withLength(min: 3, max: 15)();
+  TextColumn get categoryTitle =>
+      text().withLength(min: 3, max: 15).customConstraint('UNIQUE')();
   IntColumn get numberOfList => integer().nullable()();
   BoolColumn get isFavorite => boolean()();
   IntColumn get color => integer().nullable()();
+
+  @override
+  Set<Column> get primaryKey => {categoryId};
 }
 
 class ToDoList extends Table {
   IntColumn get listId => integer().autoIncrement()();
-  TextColumn get title => text().withLength(min: 3, max: 15)();
+  TextColumn get title =>
+      text().withLength(min: 3, max: 15).customConstraint('UNIQUE')();
   TextColumn get time => text().withLength(min: 4, max: 5)();
   TextColumn get date => text().withLength(min: 7, max: 9)();
   TextColumn get frequency => text().withLength(min: 1, max: 100)();
@@ -21,7 +30,18 @@ class ToDoList extends Table {
   BoolColumn get isCompleted => boolean()();
   TextColumn get steps => text().withLength(min: 0, max: 150)();
   BoolColumn get isImportant => boolean()();
+
+  @override
+  Set<Column> get primaryKey => {listId};
 }
 
 @UseMoor(tables: [Category, ToDoList])
 class AppDatabase {}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
+    return VmDatabase(file);
+  });
+}
