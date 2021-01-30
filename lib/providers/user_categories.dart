56076761ed7613e1e02273c1d6f8f638fbe2760f
Adedoyin <isradeoluwa@gmail.com>
2021-01-30 11:51:22 +0100

    User Category data changed in provider

--- lib/providers/user_categories.dart
+++ lib/providers/user_categories.dart
@@ -1,25 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:moor/moor.dart';
 import 'package:my_todo_app/model/app_database.dart';
 import '../model/category.dart';
 
 class UserCategories with ChangeNotifier {
-  List<UserCategory> _userCat = [];
+  List<dynamic> _userCat = [];
 
-  List<UserCategory> get userCat {
+  List<dynamic> get userCat {
     return [..._userCat];
   }
 
   void catDetail(int bgColorNum, String catName) {
-    _userCat.add(
-      UserCategory(
-          categoryTitle: catName,
-          numberOfList: 0,
-          categoryId: 0,
-          isFavorite: 0,
-          color: bgColorNum),
-    );
-    // AppDatabase().insertCategory(CategoriesCompanion(categoryTitle: catName, color: bgColorNum, isFavorite: 0, numberOfList: 0);
+    // _userCat.add(
+    // //   UserCategory(
+    // //       categoryTitle: catName,
+    // //       numberOfList: 0,
+    // //       categoryId: 0,
+    // //       isFavorite: 0,
+    // //       color: bgColorNum),
+    // // );
+    AppDatabase().insertCategory(CategoriesCompanion(
+        categoryTitle: Value(catName),
+        color: Value(bgColorNum),
+        isFavorite: Value(0)));
     // print(bgColors[bgColorNum].toString);
+
     notifyListeners();
   }
 
