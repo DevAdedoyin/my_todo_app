9bf73cda39c555ca2a7321d7610a6ace3c226a03
Adedoyin <isradeoluwa@gmail.com>
2021-02-04 09:07:05 +0100

    Category isImportant changed to a boolean from int

--- lib/model/app_database.dart
+++ lib/model/app_database.dart
@@ -14,7 +14,7 @@ class Categories extends Table {
   TextColumn get categoryTitle =>
       text().withLength(min: 3, max: 15).customConstraint('UNIQUE')();
   IntColumn get numberOfList => integer().withDefault(const Constant(0))();
-  IntColumn get isImportant => integer().withDefault(const Constant(0))();
+  BoolColumn get isImportant => boolean().withDefault(const Constant(false))();
   IntColumn get color => integer().nullable().withDefault(const Constant(0))();
 
   // @override
