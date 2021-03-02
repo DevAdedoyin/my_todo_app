dcc4db7b35d9ed8e543e33dd43a534d3178a4734
Adedoyin <isradeoluwa@gmail.com>
2021-03-02 12:16:57 +0100

    Category Screen: Snack bar and delete dao added

--- lib/widgets/user_category_widget.dart
+++ lib/widgets/user_category_widget.dart
@@ -25,84 +25,95 @@ class _UserCategoriesWidgetState extends State<UserCategoriesWidget> {
     final catDao = Provider.of<CategorieDao>(context);
     final taskDao = Provider.of<TaskDao>(context);
     print(widget.catId);
-    return InkWell(
-      onTap: () => {
-        Navigator.of(context).pushNamed(TodoListScreen.routeName,
-            arguments: [widget.title, widget.color, widget.catId])
+    return Dismissible(
+      key: Key(widget.catId.toString()),
+      onDismissed: (direction) {
+        setState(() {
+          catDao.deleteCategory(widget.catId);
+        });
+        Scaffold.of(context).showSnackBar(SnackBar(
+          content: Text(widget.title + " deleted"),
+        ));
       },
-      splashColor: widget.color,
-      borderRadius: BorderRadius.circular(10),
-      child: Card(
-        margin: EdgeInsets.only(left: 10, right: 10, top: 7, bottom: 7),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        elevation: 15,
-        shadowColor: Colors.black87,
-        child: Container(
-          height: 70,
-          alignment: Alignment.center,
-          child: ListTile(
-            leading: IconButton(
-              icon: widget.isImportant
-                  ? Icon(
-                      Icons.star,
-                      size: 20,
-                      color: widget.color,
-                    )
-                  : Icon(
-                      Icons.star_border,
-                      size: 25,
-                      color: Colors.black,
-                    ),
-              onPressed: () {
-                if (widget.isImportant == true) {
-                  catDao.updateCategoryImportance(
-                      Categorie(id: widget.catId, isImportant: false)
-                          .copyWith(id: widget.catId));
-                } else {
-                  catDao.updateCategoryImportance(
-                      Categorie(id: widget.catId, isImportant: true)
-                          .copyWith(id: widget.catId));
-                }
-              },
-              splashColor: widget.color,
-            ),
-            title: Text(
-              widget.title,
-              style: TextStyle(fontSize: 20),
-            ),
-            trailing: StreamBuilder(
-              stream: taskDao.getSpecificTask(widget.catId),
-              builder: (_, snapshot) {
-                print('DATA: ' + snapshot.data.length.toString());
+      child: InkWell(
+        onTap: () => {
+          Navigator.of(context).pushNamed(TodoListScreen.routeName,
+              arguments: [widget.title, widget.color, widget.catId])
+        },
+        splashColor: widget.color,
+        borderRadius: BorderRadius.circular(10),
+        child: Card(
+          margin: EdgeInsets.only(left: 10, right: 10, top: 7, bottom: 7),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          elevation: 15,
+          shadowColor: Colors.black87,
+          child: Container(
+            height: 70,
+            alignment: Alignment.center,
+            child: ListTile(
+              leading: IconButton(
+                icon: widget.isImportant
+                    ? Icon(
+                        Icons.star,
+                        size: 20,
+                        color: widget.color,
+                      )
+                    : Icon(
+                        Icons.star_border,
+                        size: 25,
+                        color: Colors.black,
+                      ),
+                onPressed: () {
+                  if (widget.isImportant == true) {
+                    catDao.updateCategoryImportance(
+                        Categorie(id: widget.catId, isImportant: false)
+                            .copyWith(id: widget.catId));
+                  } else {
+                    catDao.updateCategoryImportance(
+                        Categorie(id: widget.catId, isImportant: true)
+                            .copyWith(id: widget.catId));
+                  }
+                },
+                splashColor: widget.color,
+              ),
+              title: Text(
+                widget.title,
+                style: TextStyle(fontSize: 20),
+              ),
+              trailing: StreamBuilder(
+                stream: taskDao.getSpecificTask(widget.catId),
+                builder: (_, snapshot) {
+                  print('DATA: ' + snapshot.data.length.toString());
 
-                if (snapshot.hasData) {
-                  return Container(
-                    width: 50,
-                    // color: Colors.amberAccent,
-                    child: Row(
-                      children: [
-                        Expanded(
-                          flex: 3,
-                          child: IconButton(
-                            icon: Icon(Icons.edit),
-                            onPressed: () {},
+                  if (snapshot.hasData) {
+                    return Container(
+                      width: 50,
+                      // color: Colors.amberAccent,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 3,
+                            child: IconButton(
+                              icon: Icon(Icons.edit),
+                              onPressed: () {},
+                            ),
                           ),
-                        ),
-                        Expanded(
-                          child: Text(
-                            snapshot.data.length.toString(),
-                            style: TextStyle(fontSize: 15),
+                          Expanded(
+                            child: Text(
+                              snapshot.data.length.toString(),
+                              style: TextStyle(fontSize: 15),
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  );
-                } else {
-                  return Text('0');
-                }
-              },
+                        ],
+                      ),
+                    );
+                  } else {
+                    return Text('0');
+                  }
+                },
+              ),
             ),
           ),
         ),
